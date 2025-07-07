afc09acc589c11b6a5b22854f3c0175885613931
Francisco Beese <alexandrobeese@hotmail.com>
2025-07-07 08:05:20 -0300

    Fix Route Error

--- client/src/lib/paths.ts
+++ client/src/lib/paths.ts
@@ -1,14 +1,4 @@
-// export function base(path?: string): string {
-//   const base = import.meta.env.MODE === 'production' ? '/vipsrl' : '';
-//   return `${base}${path}`;
-// }
-export function base(path = ''): string {
-  const prefix = '/vipsrl';
-  const isProd = import.meta.env.MODE === 'production';
-
-  // Si no estamos en producción, devolvemos el path tal cual
-  if (!isProd) return path;
-
-  // Si ya empieza con /vipsrl, lo dejamos como está
-  return path.startsWith(prefix) ? path : `${prefix}${path}`;
+export function base(path?: string): string {
+  //const base = import.meta.env.MODE === 'production' ? '/vipsrl' : '';
+  return `${base}${path}`;
 }
